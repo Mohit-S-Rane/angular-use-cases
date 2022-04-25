c69adcb05236b668dee3cfe904c5806d36c5338e
Mohit-S-Rane <67183798+Mohit-S-Rane@users.noreply.github.com>
2022-04-25 20:03:20 +0530

    Complete Form Validation with Login Page Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,17 +4,24 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-use-cases';
-
   loginForm: FormGroup;
 
   constructor() {
     this.loginForm = new FormGroup({
-      email: new FormControl(null, [Validators.required]),
-      password: new FormControl(null, [Validators.required])
-    })
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.maxLength(8),
+        Validators.minLength(4),
+      ]),
+    });
+  }
+
+  login() {
+    console.log(this.loginForm.value);
   }
 }
