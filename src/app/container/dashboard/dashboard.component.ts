da513df2758d51ccde876b896b04a49cee3216c3
Mohit-S-Rane <67183798+Mohit-S-Rane@users.noreply.github.com>
2022-05-10 23:28:58 +0530

    ContentChild & ContentChildren with examples

--- src/app/container/dashboard/dashboard.component.ts
+++ src/app/container/dashboard/dashboard.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ContentChild, ContentChildren, ElementRef, QueryList } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
 })
-export class DashboardComponent {
-
+export class DashboardComponent implements AfterViewInit{
+  @ContentChild('myDiv') myTab: ElementRef;
+  @ContentChildren('myDiv2') myTab2: QueryList<any>;
   constructor() {}
+
+  ngAfterViewInit(): void {
+    console.log(this.myTab);
+    console.log(this.myTab2);
+    
+  }
 }
