05f4c34ae458e9a9ddd742924dfe5767e144ca93
Mohit-S-Rane <mohitsrane16@gmail.com>
2022-05-11 11:15:37 +0530

    Upload Image Logic

--- src/app/container/on-boarding/tabs/upload-image/upload-image.component.ts
+++ src/app/container/on-boarding/tabs/upload-image/upload-image.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AlertService } from 'src/app/services/alert-service';
 
 @Component({
   selector: 'app-upload-image',
@@ -9,14 +10,32 @@ export class UploadImageComponent {
   isUploaded = false;
   isSelected = false;
   selectButtonIcon = 'add';
+  file: File;
+  MAX_IMAGE_SIZE = 2 * 1000 * 1000;
+  url = '';
 
   @ViewChild('fileInput') fileInput: ElementRef;
+  @ViewChild('previewImg') previewImg: ElementRef;
 
-  constructor() {}
+  constructor(private alertService: AlertService) {}
 
-  onImageSelect(value: any) {}
+  onImageSelect(value: any) {
+    const file = value.target.files[0];
+    this.file = file;
+    if (this.file.size > this.MAX_IMAGE_SIZE){
+        return this.alertService.error('File size should be less than 2MB')  
+    }
+
+    if(file.type === 'image/png' || 'image/jpg' || 'image/jpeg' || 'image/JPG'){
+        this.isSelected = true;
+        this.selectButtonIcon = 'cached';
+        this.previewImg.nativeElement.src = window.URL.createObjectURL(this.file);
+    } else {
+        return this.alertService.error('Image must be of type png, jpg or jpeg')
+    }
+  }
 
   onFileSelect() {
-    this.fileInput.nativeElement.click(); 
+    this.fileInput.nativeElement.click();
   }
 }
