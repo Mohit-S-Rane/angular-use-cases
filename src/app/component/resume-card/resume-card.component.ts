a8388da2e3e4ed7aa339882889a8b87b9b046651
Mohit-S-Rane <mohitsrane16@gmail.com>
2022-05-30 15:26:24 +0530

    Template Page

--- src/app/component/resume-card/resume-card.component.ts
+++ src/app/component/resume-card/resume-card.component.ts
@@ -4,6 +4,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { AddOrEditResumeComponent } from './../dialogues/add-or-edit-resume/add-or-edit-resume.component';
 import { ResumeRepository } from 'src/app/repository/resume-repository';
 import { AlertService } from './../../services/alert-service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-resume-card',
   templateUrl: './resume-card.component.html',
@@ -12,7 +13,7 @@ import { AlertService } from './../../services/alert-service';
 export class ResumeCardComponent {
   @Input() resume: Resume;
   hover = false;
-  constructor(private matDialog: MatDialog, private resumeRepo: ResumeRepository, private alertService: AlertService) {}
+  constructor(private matDialog: MatDialog, private resumeRepo: ResumeRepository, private alertService: AlertService, private router: Router) {}
 
   editResume() {
     this.matDialog.open(AddOrEditResumeComponent, {
@@ -24,5 +25,9 @@ export class ResumeCardComponent {
     this.resumeRepo.deleteResume(this.resume._id).subscribe(data => {
       this.alertService.success('Resume deleted successfully')
     })
+  }
+
+  download() {
+    this.router.navigate(['dashboard', 'resume', 'template', 'this.resume._id'])
   }
 }
--- src/app/container/templates/templates.component.ts
+++ src/app/container/templates/templates.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { Utility } from './../../utility/utility';
 @Component({
   selector: 'app-templates',
   templateUrl: './templates.component.html',
-//   styleUrls: ['./templates.component.css'],
+  styleUrls: ['./templates.component.css'],
 })
 export class TemplatesComponent {
+  templates = Utility.Templates;
   constructor() {}
 }
